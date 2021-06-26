7855106f401ed653da5992faa4f95f6740c759d3
Dulshan <abavira@gmail.com>
2021-06-27 00:12:22 +0530

    add Lead model

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField(default=0) 
+
